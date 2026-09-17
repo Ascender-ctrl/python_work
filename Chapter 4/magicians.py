248e53812ba2ce092a461dcd95b98b725bb81455
Ascender-ctrl <abasiamek.amos@gmailcom>
2026-09-17 16:02:48 -0400

    modified:   Chapter 4/magicians.py

--- Chapter 4/magicians.py
+++ Chapter 4/magicians.py
@@ -1,6 +1,7 @@
 magicians = ['alice', 'david', 'carolina']
 
 for magician in magicians:
-    print(magician)
+    print(f"{magician.title()}, that was an awesome trick!")
+    print(f"I cannot wait to see your next trick, {magician.title()}. \n")
     
-    
+print("Thank you, everyone. That was a great magic show!")
