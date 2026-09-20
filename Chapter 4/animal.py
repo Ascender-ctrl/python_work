f868b5cbc4de2acaa073272d35efae21c2241da9
Ascender-ctrl <abasiamek.amos@gmailcom>
2026-09-20 17:53:18 -0400

    new file:   Chapter 4/animal.py 	new file:   Chapter 4/pizza.py

---
+++ Chapter 4/animal.py
@@ -0,0 +1,6 @@
+cats = ['lions','tigers','leopards']
+
+for cat in cats:
+    print(f"{cat.title()} would not make a great pet! \n")
+    
+print("The reason is simply because they're wild animals! Big cats! Apex Predators!")
---
+++ Chapter 4/pizza.py
@@ -0,0 +1,8 @@
+#exercise 4-1
+
+pizzas = ['bbq pizza','pepperoni pizza','mozzarella cheese pizza']
+
+for pizza in pizzas:
+    print(f"{pizza.title()} is one of top 3 pizzas \n")
+    
+print("These three are for sure my favorite pizzas!")
